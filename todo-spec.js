b9932214d09178c3a04b8fc35176ba43cb9e44b9
Guilherme Gorski <guilhermevgorski@gmail.com>
2020-01-14 12:45:58 -0300

    first commit

---
+++ todo-spec.js
@@ -0,0 +1,25 @@
+describe('GIVEN I am at NodeJS home page', function() {
+  it('WHEN I click on docs link', function() {
+    let expectedConditions = protractor.ExpectedConditions;
+
+    browser.waitForAngularEnabled(false)
+    browser.get('https://nodejs.org/en/');
+    
+    let docsLink = element(By.css('a[href="/en/docs/"]'));
+    let guidesLink = element(By.css('a[href="/en/docs/guides/"]'));
+    let gettinStartedLink = element(By.css('a[href="/en/docs/guides/getting-started-guide/"]'));
+    
+      //WHEN I click on docs link at home page
+    docsLink.click();
+      //AND click on guides link at docs page
+    guidesLink.click();
+      //AND I click on Getting Started link at guides page
+    gettinStartedLink.click();
+      //THEN I should see a JavaScript code in a board
+    expect(expectedConditions.visibilityOf($('pre[class="language-javascript"]')), 5000);
+  });
+});
+
+
+ 
+
